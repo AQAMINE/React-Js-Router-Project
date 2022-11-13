d1c4eb3dd4c1f69fc0d59575dbee176aabef986c
amine aqebli <aqebli36@gmail.com>
2022-11-13 01:15:43 +0100

    Adding Dummy Data & More Content

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,15 @@
+import QuoteList from '../components/quotes/QuoteList';
+
+const DUMMY_QUOTES = [
+  {id: 'q1', author: 'Oscar Wilde', text: 'Be yourself; everyone else is already taken.'},
+  {id: 'q2', author: 'Frank Zappa', text: 'So many books, so little time.'},
+  {id: 'q3', author: 'Marcus Tullius Cicero', text: 'A room without books is like a body without a soul.'},
+  {id: 'q4', author: 'Mae West', text: 'You only live once, but if you do it right, once is enough.'},
+  {id: 'q5', author: 'Mahatma Gandhi', text: 'Be the change that you wish to see in the world.'}
+]
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />
 };
 
 export default AllQuotes;
