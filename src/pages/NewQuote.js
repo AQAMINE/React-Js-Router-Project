ad8a708f76c6d34a08a7b6c0b4f016537b55df31
amine aqebli <aqebli36@gmail.com>
2022-11-13 18:43:14 +0100

    Imperative Navigation Using useHistory Hook

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,9 +1,16 @@
+import { useHistory } from 'react-router-dom';
+
 import QuoteForm from '../components/quotes/QuoteForm';
-    const addQuoteHandler = quoteData => {
-        console.log(quoteData)
-    }
 
 const NewQuote = () => {
+    const history = useHistory();
+
+    const addQuoteHandler = quoteData => {
+        console.log(quoteData)
+
+        history.push('/quotes');
+    }
+
     return <QuoteForm onAddQuote={addQuoteHandler} />;
 }
 
