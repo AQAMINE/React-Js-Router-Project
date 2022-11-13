92eaa8d717ac3b3950b5bb93edec8e28f025a512
amine aqebli <aqebli36@gmail.com>
2022-11-13 17:34:48 +0100

    Outputting Data Using Route Params

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,16 +1,30 @@
 import { useParams,Route } from "react-router-dom";
 import { Fragment } from "react";
 
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
+
+const DUMMY_QUOTES = [
+  {id: 'q1', author: 'Oscar Wilde', text: 'Be yourself; everyone else is already taken.'},
+  {id: 'q2', author: 'Frank Zappa', text: 'So many books, so little time.'},
+  {id: 'q3', author: 'Marcus Tullius Cicero', text: 'A room without books is like a body without a soul.'},
+  {id: 'q4', author: 'Mae West', text: 'You only live once, but if you do it right, once is enough.'},
+  {id: 'q5', author: 'Mahatma Gandhi', text: 'Be the change that you wish to see in the world.'}
+]
 
 const QuoteDetail = () => {
   const params = useParams();
   const { quoteId } = params;
 
+  const quote = DUMMY_QUOTES.find(quote => quote.id === quoteId);
+
+  if(!quote){
+    return <p>No quote found!</p>
+  }
+
   return (
     <Fragment>
-      <h1>Quote Detail Page</h1>
-      <p>{quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${quoteId}/comments`}>
         <Comments />
       </Route>
