7ef82cea2deefa7a90e4f44dfad56f3c9279fecc
aqamine <32686488+AQAMINE@users.noreply.github.com>
2023-02-21 01:07:41 +0100

    last

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,4 +1,4 @@
-import { useParams,Route, Link } from "react-router-dom";
+import { useParams,Route, Link, useRouteMatch } from "react-router-dom";
 import { Fragment } from "react";
 
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
@@ -13,6 +13,7 @@ const DUMMY_QUOTES = [
 ]
 
 const QuoteDetail = () => {
+  const match = useRouteMatch();
   const params = useParams();
   const { quoteId } = params;
 
@@ -25,13 +26,13 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${quoteId}`} exact>
+      <Route path={`${match.path}`} exact>
           <div className="centered">
-            <Link className="btn--flat" to={`/quotes/${quoteId}/comments`}>Load Comments</Link>
+            <Link className="btn--flat" to={`${match.url}/comments`}>Load Comments</Link>
           </div>
       </Route>
       
-      <Route path={`/quotes/${quoteId}/comments`}>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </Fragment>
